fdbc843f6fb48066a3e8f2704f14aca640781b0a
RenatoL <boiller.l@gmail.com>
2017-11-07 16:52:17 +0000

    Customers table populated with Faker data

--- db/structure.sql
+++ db/structure.sql
@@ -30,40 +30,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: customers; Type: TABLE; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE TABLE customers (
-    id integer NOT NULL,
-    first_name character varying(255) NOT NULL,
-    last_name character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    username character varying(255) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
-);
-
-
---
--- Name: customers_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE customers_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: customers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE customers_id_seq OWNED BY customers.id;
-
-
---
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -117,22 +83,7 @@ ALTER SEQUENCE users_id_seq OWNED BY users.id;
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY customers ALTER COLUMN id SET DEFAULT nextval('customers_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
-
-
---
--- Name: customers_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
---
-
-ALTER TABLE ONLY customers
-    ADD CONSTRAINT customers_pkey PRIMARY KEY (id);
 
 
 --
@@ -141,20 +92,6 @@ ALTER TABLE ONLY customers
 
 ALTER TABLE ONLY users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE UNIQUE INDEX index_customers_on_email ON customers USING btree (email);
-
-
---
--- Name: index_customers_on_username; Type: INDEX; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE UNIQUE INDEX index_customers_on_username ON customers USING btree (username);
 
 
 --
@@ -189,6 +126,4 @@ INSERT INTO schema_migrations (version) VALUES ('20171103152801');
 INSERT INTO schema_migrations (version) VALUES ('20171106115034');
 
 INSERT INTO schema_migrations (version) VALUES ('20171106115035');
-
-INSERT INTO schema_migrations (version) VALUES ('20171106151716');
 
